1019e114008ea1990a68359e83e397ddffac8eef
Brian Campbell <brian.campbell003@gmail.com>
2022-09-19 22:51:52 -0500

    file structure

---
+++ index.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+const Employee = require('./lib/Employee.js');
+const Manager = require('./lib/Manager.js');
+const Engineer = require('./lib/Engineer.js');
+const Intern = require('./lib/Intern.js');
